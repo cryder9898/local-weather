37d579e1b0759d0d55bbe829c031548dbc739877
Christopher Ryder <cryder9898@gmail.com>
2017-07-12 23:40:22 -0400

    fixed styling and updated javascript

--- js/script.js
+++ js/script.js
@@ -1,50 +1,49 @@
-$(document).ready(function() {
-  
-  var apiKey = "4d7005e65cf53c37",
-  tempF = "",
-  tempC = "";
-  
-  function getWeather(city, state) {
-    $.ajax({
-      url: "https://api.wunderground.com/api/" + apiKey + "/conditions/q/" + state + "/" + city + ".json",
-      dataType: "jsonp",
-      success: function(parsed_json) {
-        var forecast = parsed_json['current_observation'];
-        tempC = forecast['temp_c'];
-        tempF = forecast['temp_f'];
-        $('#temp').text(tempF);
-        $('#type').text("F");
-        $('#imgURL').attr('src',forecast['icon_url']);
-      }
-    });
-  }
+const apiKey = "4d7005e65cf53c37";
 
-  function switchTemp(type) {
-    if ($('#type').text() == 'F') {
-      $('#type').text('C');
-      $('#temp').text(tempC);
-    } else {
-      $('#type').text('F');
+const getWeather = function(city, state) {
+  $.ajax({
+    url: "https://api.wunderground.com/api/" + apiKey + "/conditions/q/" + state + "/" + city + ".json",
+    dataType: "jsonp",
+    success: data => {
+      var forecast = data['current_observation'];
+      tempC = forecast['temp_c'];
+      tempF = forecast['temp_f'];
       $('#temp').text(tempF);
+      $('#image').attr('src',forecast['icon_url']);
     }
+  });
+}
+
+const switchTemp = (type) => {
+  if ($('#type').text() === 'F') {
+    $('#type').text('C');
+    $('#temp').text(tempC);
+  } else {
+    $('#type').text('F');
+    $('#temp').text(tempF);
   }
-  
+}
+
+$(document).ready(() => {
+
+  let tempF = "";
+  let tempC = "";
+
   $.ajax({
     url: "https://api.wunderground.com/api/" + apiKey + "/geolookup/q/autoip.json",
     dataType: "jsonp",
-    success: function(parsed_json) {
-      
-      var local = parsed_json['location'];
-      
-      $('#city').text(local['city']);
-      $('#state').text(local['state']);
-      getWeather(parsed_json.location.city, parsed_json.location.state);
-      
+    success: data => {
+      let local = data['location'];
+      $('#city').text(local.city);
+      $('#state').text(local.state);
+      getWeather(local.city, local.state);
+
     }
   });
-  
-  $('#type').click(function () {
+
+  $('#type').text("F");
+  $('#type').click(() => {
     switchTemp($('#type').text());
   });
-  
+
 });
